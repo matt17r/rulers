775496d8c09a9003c8ddca941f748da1f940061e
Matthew Lindfield Seager <matthew@sagacity.com.au>
2020-06-05 14:35:28 +1000

    Add helper method to convert string to snake_case

--- lib/rulers/util.rb
+++ lib/rulers/util.rb
@@ -1,3 +1,13 @@
+module Rulers
+  def self.to_underscore(string)
+    string.gsub(/::/, '/').
+      gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2').
+      gsub(/([a-z\d])([A-Z])/, '\1_\2').
+      tr('-', '_').
+      downcase
+  end
+end
+
 class Object
   def blank?
     respond_to?(:empty?) ? !!empty? : !self
